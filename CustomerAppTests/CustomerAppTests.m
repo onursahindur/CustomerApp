2c5036f4b4d79f1736f8d995faf9fcb4772e60e9
Onur Şahindur <onur@mobilike.com>
2016-11-11 13:19:18 +0200

    Added accessToken test case

--- CustomerAppTests/CustomerAppTests.m
+++ CustomerAppTests/CustomerAppTests.m
@@ -7,33 +7,39 @@
 //
 
 #import <XCTest/XCTest.h>
+#import "CASubmissionsViewController.h"
+#import "CANetworkManager.h"
+#import "CAManager.h"
 
 @interface CustomerAppTests : XCTestCase
+
+@property (nonatomic, strong) CASubmissionsViewController *subVC;
 
 @end
 
 @implementation CustomerAppTests
 
-- (void)setUp {
+- (void)setUp
+{
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    self.subVC = [CASubmissionsViewController new];
 }
 
-- (void)tearDown {
+- (void)tearDown
+{
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testAccessTokenExists
+{
+    [[CANetworkManager sharedInstance] getProjectListWithSuccessBlock:^(NSArray *projectList)
+    {
+        XCTAssertNotNil([CAManager sharedInstance].accessToken);
+    } failureBlock:nil];
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
+
+
 
 @end
